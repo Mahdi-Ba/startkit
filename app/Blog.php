ae1a54c4031968f59865f8823aa7484d22a7ca6e
Mahdi Bahari <baharimahdi93@gmail.com>
2019-09-27 20:25:09 +0330

    blog created complete

---
+++ app/Blog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+
+class Blog extends Model
+{
+    use HasTags;
+
+    protected $fillable = ['title','slug','content','category_id','img'];
+
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+}
---
+++ database/migrations/2019_09_27_171800_create_blogs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->string('img');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign(['user_id']);
+        Schema::dropIfExists('blogs');
+    }
+}
