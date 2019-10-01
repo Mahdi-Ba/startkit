6dc89cd7cc181ecee6b4ba1d0caba43a8b62b216
Mahdi Bahari <baharimahdi93@gmail.com>
2019-10-01 22:59:22 +0330

    .

--- resources/views/admin/blog/list.blade.php
+++ resources/views/admin/blog/list.blade.php
@@ -146,51 +146,7 @@
                                 .catch(function (error) {
                                     self.responseAlert(false);
                                 });
-                          <div class="col-lg-4 col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Nestable1</h4>
-                            <div class="myadmin-dd dd" id="nestable">
-                                <ol class="dd-list">
-                                    <li class="dd-item" data-id="1">
-                                        <div class="dd-handle"> Item 1 </div>
-                                    </li>
-                                    <li class="dd-item" data-id="2">
-                                        <div class="dd-handle"> Item 2 </div>
-                                        <ol class="dd-list">
-                                            <li class="dd-item" data-id="3">
-                                                <div class="dd-handle"> Item 3 </div>
-                                            </li>
-                                            <li class="dd-item" data-id="4">
-                                                <div class="dd-handle"> Item 4 </div>
-                                            </li>
-                                            <li class="dd-item" data-id="5">
-                                                <div class="dd-handle"> Item 5 </div>
-                                                <ol class="dd-list">
-                                                    <li class="dd-item" data-id="6">
-                                                        <div class="dd-handle"> Item 6 </div>
-                                                    </li>
-                                                    <li class="dd-item" data-id="7">
-                                                        <div class="dd-handle"> Item 7 </div>
-                                                    </li>
-                                                    <li class="dd-item" data-id="8">
-                                                        <div class="dd-handle"> Item 8 </div>
-                                                    </li>
-                                                </ol>
-                                            </li>
-                                            <li class="dd-item" data-id="9">
-                                                <div class="dd-handle"> Item 9 </div>
-                                            </li>
-                                            <li class="dd-item" data-id="10">
-                                                <div class="dd-handle"> Item 10 </div>
-                                            </li>
-                                        </ol>
-                                    </li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
-                </div>  }
+                        }
                     });
 
                 }
